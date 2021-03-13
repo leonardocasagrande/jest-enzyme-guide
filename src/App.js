5e5fd2b681590f20b7c6dd522bca503ddf6c467e
Leonardo Casagrande <leoc.casagrande@hotmail.com>
2021-03-13 19:35:08 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [displayError, setDisplayError] = useState(false);
+
+  const changeCountHandler = (val) => {
+    if(count + val < 0) {
+      setDisplayError(true);
+    } else {
+      setCount(count + val);
+      setDisplayError(false);
+    }
+  }
+
   return (
     <div data-test="component-app">
-      <h1>App</h1>
+      <h1 data-test="counter-display">
+        The counter is currently&nbsp;
+        <span data-test="count">
+          {count}
+        </span>
+      </h1>
+      {displayError && (
+        <h2 data-test="error">Counter can't go below 0</h2>
+      )}
+      <button
+        onClick={() => changeCountHandler(1)}
+        data-test="increment-button"
+      >Increment counter</button>
+      <button
+        onClick={() => changeCountHandler(-1)}
+        data-test="decrement-button" >
+        Decrement counter
+        </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,77 @@
 import App from './App';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+/**
+ * Factory function to create a ShallowWrapper for the App component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
+
 test("renders without errors", () => {
-  const wrapper = shallow(<App />);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, "component-app")
   expect(appComponent.length).toBe(1);
 });
 test("renders button", () => {
-
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button")
+  expect(button.length).toBe(1);
 })
 test("renders counter display", () => {
-  
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, "counter-display")
+  expect(counterDisplay.length).toBe(1);
 })
 test("counter starts at 0", () => {
-  
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
 })
-test("clicking on button increments counter display", () => {
-  
+test("clicking on increment button increments counter display", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "increment-button");
+  button.simulate('click');
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+})
+
+test("clicking on decrement button while 0 displays error", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "decrement-button");
+  button.simulate('click');
+  const error = findByTestAttr(wrapper, "error");
+  expect(error.length).toBe(1);
+})
+
+test("clicking on decrement button while 0 still displays 0", () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, "decrement-button");
+  button.simulate('click');
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("0");
+})
+
+test("clicking on decrement button decrement counter display", () => {
+  const wrapper = setup();
+  const decButton = findByTestAttr(wrapper, "decrement-button");
+  const incButton = findByTestAttr(wrapper, "increment-button");
+  incButton.simulate('click');
+  incButton.simulate('click');
+  decButton.simulate('click');
+  const count = findByTestAttr(wrapper, "count").text();
+  expect(count).toBe("1");
+})
+test("clicking on increment while error is displayed remove error", () => {
+  const wrapper = setup();
+  const decButton = findByTestAttr(wrapper, "decrement-button");
+  const incButton = findByTestAttr(wrapper, "increment-button");
+  decButton.simulate('click');
+  incButton.simulate('click');
+  const error = findByTestAttr(wrapper, "error");
+  expect(error.length).toBe(0);
 })
